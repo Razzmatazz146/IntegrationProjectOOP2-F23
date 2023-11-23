37f722259276b7e2cb4e46de94a117e07d3c65b6
Mx.Chetty <Le1727424@crcmail.net>
2023-11-23 18:05:52 -0500

    Added Classes

--- src/main/java/com/example/integration_project_oop2/Classes/Login.java
+++ src/main/java/com/example/integration_project_oop2/Classes/Login.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class Login extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Login.class.getResource("client-login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Login.class.getResource("user-login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
