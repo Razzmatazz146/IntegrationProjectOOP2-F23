956f77b6c655971de72960ac6b1e55451f66ddbe
Mx.Chetty <Le1727424@crcmail.net>
2023-11-24 17:33:16 -0500

    added back buttons to all required views

--- src/main/java/com/example/integration_project_oop2/Controllers/ShowtimesListEditorController.java
+++ src/main/java/com/example/integration_project_oop2/Controllers/ShowtimesListEditorController.java
@@ -1,4 +1,15 @@
 package com.example.integration_project_oop2.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
 public class ShowtimesListEditorController {
+
+    @FXML
+    protected void onBackButtonClick(ActionEvent actionEvent) {
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.close();
+    }
 }
