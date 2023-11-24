ea227165e8ac74d51e095a11348fa812f85c7464
Razzmatazz146 <le1935607@crc-lennox.qc.ca>
2023-11-24 14:26:26 -0500

    Assigned Signup and Login buttons. Added a placeholder for manager/client login. Empty string in Username opens client view.

--- src/main/java/com/example/integration_project_oop2/Controllers/LoginController.java
+++ src/main/java/com/example/integration_project_oop2/Controllers/LoginController.java
@@ -1,5 +1,63 @@
 package com.example.integration_project_oop2.Controllers;
 
-public class LoginController {
+import com.example.integration_project_oop2.MovieTheaterApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+public class LoginController {
+    public PasswordField passwordTextField;
+    public Button loginButton;
+    public Button signupButton;
+    public Button managementButton;
+    public Button userButton;
+    public TextField usernameTextField;
+
+
+    private String checkManager(){
+
+        String selectedView;
+
+        if (usernameTextField.getText().isEmpty()){
+            selectedView = "client-view.fxml";
+        } else {
+            selectedView = "manager-view.fxml";
+        }
+        return selectedView;
+    }
+
+    @FXML
+    protected void onLoginButtonClick(ActionEvent pEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MovieTheaterApplication.class.getResource(checkManager()));
+        Parent view = fxmlLoader.load();
+        Scene nextScene = new Scene(view);
+        Stage nextStage = new Stage();
+        nextStage.setScene(nextScene);
+        nextStage.setTitle("Available Movies");
+        nextStage.initModality(Modality.WINDOW_MODAL);
+        nextStage.initOwner(((Node) pEvent.getSource()).getScene().getWindow());
+        nextStage.showAndWait();
+    }
+    @FXML
+    protected void onSignUpButtonClick(ActionEvent pEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MovieTheaterApplication.class.getResource("user-signup.fxml"));
+        Parent view = fxmlLoader.load();
+        Scene nextScene = new Scene(view);
+        Stage nextStage = new Stage();
+        nextStage.setScene(nextScene);
+        nextStage.setTitle("Sign Up");
+        nextStage.initModality(Modality.WINDOW_MODAL);
+        nextStage.initOwner(((Node) pEvent.getSource()).getScene().getWindow());
+        nextStage.showAndWait();
+    }
 }
