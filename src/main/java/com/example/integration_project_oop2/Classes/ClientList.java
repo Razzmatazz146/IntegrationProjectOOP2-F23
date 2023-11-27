989765a7e35f14af02cc326317775ac478acb6f4
Mx.Chetty <Le1727424@crcmail.net>
2023-11-27 09:42:56 -0500

    started ClientList

--- src/main/java/com/example/integration_project_oop2/Classes/ClientList.java
+++ src/main/java/com/example/integration_project_oop2/Classes/ClientList.java
@@ -1,4 +1,10 @@
 package com.example.integration_project_oop2.Classes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClientList {
+    private List<Client> aClientList;
+
+    public ClientList() { this.aClientList = new ArrayList<>(); }
 }
