7ca51e07283eb5a62734d32cc496aa7d64a2c5b1
Mx.Chetty <Le1727424@crcmail.net>
2023-12-04 10:25:29 -0500

    fixed commit

--- src/main/java/com/example/integration_project_oop2/Controllers/ShowtimesListEditorController.java
+++ src/main/java/com/example/integration_project_oop2/Controllers/ShowtimesListEditorController.java
@@ -1,5 +1,6 @@
 package com.example.integration_project_oop2.Controllers;
 
+import com.example.integration_project_oop2.Classes.Movie;
 import com.example.integration_project_oop2.Classes.Showtime;
 import com.example.integration_project_oop2.Classes.ShowtimeList;
 import com.example.integration_project_oop2.Classes.SingletonLists;
@@ -11,6 +12,8 @@ import javafx.stage.Stage;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
+
+import static com.example.integration_project_oop2.Controllers.WindowController.newWindow;
 
 public class ShowtimesListEditorController {
 
@@ -24,6 +27,7 @@ public class ShowtimesListEditorController {
     public ChoiceBox movieDropdown;
     public ChoiceBox showroomDropdown;
     public ChoiceBox timeDropdown;
+
     private ShowtimeList showtimeList;
 
     private String getSelectedMovie() { return  (String) movieDropdown.getSelectionModel().getSelectedItem(); }
@@ -37,6 +41,12 @@ public class ShowtimesListEditorController {
     };
 
     @FXML
+    protected void onBackButtonClick(ActionEvent actionEvent) {
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
     private void initialize(){
         SingletonLists lists = SingletonLists.getInstance();
         showtimeList = lists.getShowtimeList();
@@ -48,20 +58,15 @@ public class ShowtimesListEditorController {
         }
     }
 
-    @FXML
-    protected void onBackButtonClick(ActionEvent actionEvent) {
-        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        stage.close();
-    }
-
-    public void onAddButtonClick() {
+    public void onAddButtonClick(ActionEvent event) {
         String realMovie = "It";
         int realRoom = 1;
 
         String selectedMovie = (String) movieDropdown.getSelectionModel().getSelectedItem();
         int selectedRoom = (int) showroomDropdown.getSelectionModel().getSelectedItem();
 
-        //TODO Add code to add new movie using fields.
+
+        newWindow(event, "addShowtime-view.fxml", "Add Showtime");
 
         if (movieDropdown.getSelectionModel().getSelectedItem().equals(realMovie) && showroomDropdown.getSelectionModel().getSelectedItem().equals(realRoom)){
             Alert viewAlert = new Alert(Alert.AlertType.ERROR, "This showtime already exists.");
@@ -75,7 +80,7 @@ public class ShowtimesListEditorController {
         }
     }
 
-    public void onUpdateButtonClick() {
+    public void onUpdateButtonClick(ActionEvent event) {
 
         if(showtimesListView.getSelectionModel().isEmpty()) {
             Alert viewAlert = new Alert(Alert.AlertType.ERROR, "Select a showtime.");
@@ -83,6 +88,9 @@ public class ShowtimesListEditorController {
         } else {
 
             // TODO: Add code to update database List.
+            String title = showtimesListView.getSelectionModel().getSelectedItem().toString();
+            newWindow(event,"addShowtime-view.fxml", "Update " + title);
+
 
             Alert viewAlert = new Alert(Alert.AlertType.CONFIRMATION, "Showtime for "+ getSelectedMovie() + "and " + getSelectedRoom() +" has been updated.");
             viewAlert.showAndWait();
@@ -97,6 +105,12 @@ public class ShowtimesListEditorController {
         } else {
 
             // TODO Add code to remove item from ListView and database list.
+            Showtime selectedShowtime = showtimeList.getShowtimeByIndex(showtimesListView.getSelectionModel().getSelectedIndex());
+
+            showtimesListView.getItems().remove(selectedShowtime);
+            showtimeList.removeShowtime(selectedShowtime);
+
+            initialize();
 
             showtimesListView.getItems().remove(getSelectedMovie());
             Alert viewAlert = new Alert(Alert.AlertType.CONFIRMATION, getSelectedMovie() + "'s showtime in " + getSelectedRoom() + " has been removed.");
